da4f254943c081d0c949701f883fb60e56c047c6
Shekar Roychowdhury <M1039139@mindtree.com>
2018-09-24 17:31:07 +0530

    Graph API is configured to fetch User country

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -12,21 +12,10 @@ export class TestComponent implements OnInit {
   public IsAuthenticated: boolean;
   private accessToken: string;
   private token;
-  // private authContext;
-  // private isCallback;
   private response: any;
   private userResponse: any;
 
   constructor(private authService: AdalService, private http: HttpClient) {
-  //   this.IsAuthenticated = false;
-  //   this.authService.init(environment.config);
-  //   this.authContext = new AuthenticationContext({
-  //     instance: 'https://login.microsoftonline.com/',
-  //     tenant: CONFIG.Settings.aadTenant,
-  //     clientId: CONFIG.Settings.spaClientId,
-  //     postLogoutRedirectUri: window.location.origin,
-  //     cacheLocation: 'localStorage',
-  // });
 
   }
 
@@ -39,58 +28,34 @@ export class TestComponent implements OnInit {
       else {
         this.IsAuthenticated = false;
       }
-      console.log(this.authService.userInfo);
-  //   this.isCallback = this.authContext.isCallback(window.location.hash);
-  //   if (this.isCallback) {
-  //     this.authContext.handleWindowCallback();
+
   // }
   }
 
 
 
   Login() {
-    // this.accessToken = this.authService.login();
-    // console.log('Login Fired!');
-    // this.authService.login();
     this.authService.login();
     this.IsAuthenticated = true;
   }
 
   Logout() {
     localStorage.clear();
-    // console.log('Logout Fired!');
     this.authService.logOut();
     this.IsAuthenticated = false;
-    // localStorage.clear();
-    // this.authService.logout();
-    // this.authService.logOut();
   }
 
   GetData() {
-    // console.log('Accessing JWT Token --->');
-    this.token = this.authService.userInfo.token;
-
-    // console.log('User Profile Details');
-    // console.log(this.authService.userInfo.profile);
-    // console.log(this.token);
-    // acquireToken('serviceClientId', function (err, res) {
-    //   console.log('Accessing Access Token --->');
-    //   this.accessToken = res;
-    //   console.log(this.accessToken);
-    // });
-    // console.log('Aquiring Access Token');
+    // this.token = this.authService.userInfo.token;
     this.authService.acquireToken(CONFIG.Settings.serviceClientId).subscribe(accessToken => {
       this.accessToken = accessToken;
-      console.log('Access Token => ' + this.accessToken);
     },
     err => {
       // console.log(this.authService.config);
       console.log(err);
     });
 
-    // console.log('trying to hit API');
      this.http.get(CONFIG.Settings.serviceUrl, this.prepareOptions()).subscribe(data => {
-      // console.log('API Hit. Response is ' + data.toString());
       this.response = data;
    }, err => {
      console.log(err);
@@ -98,29 +63,9 @@ export class TestComponent implements OnInit {
    });
   }
 
-  // GetUser() {
-  //   this.authService.acquireToken(CONFIG.Settings.spaClientId).subscribe(accessToken => {
-  //     this.accessToken = accessToken;
-  //     console.log('Access Token => ' + this.accessToken);
-  //   },
-  //   err => {
-  //     // console.log(this.authService.config);
-  //     console.log(err);
-  //   });
-
-  //   this.http.get('https://graph.microsoft.com/beta/me/', this.prepareOptions()).subscribe(data => {
-  //     console.log('API Hit. Response is ' + data.toString());
-  //     this.userResponse = data;
-  //  }, err => {
-  //    console.log(err);
-  //    this.userResponse = [ 'Did not' , 'Access', 'Graph API'];
-  //  });
-  // }
-
   prepareOptions(): any {
     let headers = new HttpHeaders();
     headers = headers
-        // .set('Content-Type', 'application/json')
         .set('Authorization', 'Bearer ' + this.accessToken);
     return { headers };
 }
--- src/assets/Config.ts
+++ src/assets/Config.ts
@@ -1,20 +1,5 @@
 export let CONFIG = {
     Settings: {
-      // BASEAPI: 'https://localhost:44356/api', // .NetCore2.0 WebAPI
-      // CLIENT_ID: 'ee3e56f1-32fd-4d13-acbc-263c4fa3a748', // https://apps.dev.microsoft.com’
-      // TENANT: 'shekarmindtree.onmicrosoft.com',
-      // TENANT_ID: '41814658-1358-4ce7-b93d-bc3fce646e07', // found in Azure->Active Directory->Properties Pane, TennantID = Directory ID
-      // AUTHORITY: 'https://login.microsoftonline.com/ee3e56f1-32fd-4d13-acbc-263c4fa3a748',
-      // RESPONSE_TYPE: 'id_token',
-      // RESPONSE_MODE: 'id_token',
-      // STATE: '',
-      // MS_GRAPH_URI: 'https://graph.microsoft.com/',
-      // REDIRECT_URI : 'http://localhost:4210/',
-      // POST_LOGOUT_REDIRECT : 'https://localhost:4210',
-      // MSGRAPH_BETA_API: 'https://graph.microsoft.com/beta/',
-      // MSGRAPH_v1_API: 'https://graph.microsoft.com/v1.0/',
-      // SCOPES: ['People.Read'], // People.Read.All - Requires an Administrator to grant this permission
-      // ADMIN_CONSENT: true
       aadTenant : 'shekarmindtree.onmicrosoft.com',
       spaClientId : 'ee3e56f1-32fd-4d13-acbc-263c4fa3a748', // AAD app client id for this app
       redirectUri: window.location.origin + '/',
